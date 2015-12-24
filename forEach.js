e7d1f92293e436ed20e52b295f55e34eedd2cb7b
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-12-24 17:53:50 +0100

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,34 +1,12 @@
 //noprotect
-function each(m,f,t){
- var c=f, o=m, e=Object(o), l=e.length>>>0;
- e=l>0;
- if(e===true){
-	e=typeof(c)==='function';
-	if(e===true){
-		for(var f=c, m=o, e=t, j=l,i=0;i<j;i++){f.call(e,m[i],i,m);};
-	}else{
-	 throw new TypeError('each callback is not a function');
-	};
- };
- return e;
-}
-
-/*
-var processed=forEach([1,2,3],alert);
-false|true
-*/
 
 var  Array=window.Array,
 $AP=Array.prototype;
 
+$AP.forEach=$AP.forEach||function(f,t){
+ var o=this, e=Object(o), i=e.length, l=i>>>0, c=f;
+ if(i>0){
+	if(typeof(c)==='function'){e=t;i=0;while(i<l){c.call(e,o[i],i,o);i++;};}else{throw new TypeError('each callback is not a function');};
+ };
+};
 
-if(!$AP.forEachz){$AP.forEachz=function(f,t){each(this,f,t);};};
-
-
-
-function logArrayElements(element, index, array) {
-  console.log('a[' + index + '] = ' + element);
-}
-[2, 5, , 9].forEachz(logArrayElements);
-
-each([2, 5, , 9],logArrayElements);
